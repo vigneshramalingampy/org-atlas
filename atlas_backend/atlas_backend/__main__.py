58ad4b0b0bc2127a98d2318ac84ef50a8150b499
vignesh <vignesh.ramalingam.py@gmail.com>
2026-07-08 16:06:18 +0530

    chore: root initialization and boilerplate structure

--- atlas_backend/atlas_backend/__main__.py
+++ atlas_backend/atlas_backend/__main__.py
@@ -2,16 +2,19 @@ import uvicorn
 
 from atlas_backend.core.settings import settings
 
+
 def main() -> None:
     """Entrypoint of the Application"""
     if settings.reload:
-        uvicorn.run("atlas_backend.core.application:app",
-                    workers=4,
-                    host=settings.host,
-                    port=settings.port,
-                    reload=settings.reload,
-                    log_level=settings.log_level.value.lower(),
-                    factory=True)
+        uvicorn.run(
+            "atlas_backend.core.application:app",
+            workers=4,
+            host=settings.host,
+            port=settings.port,
+            reload=settings.reload,
+            log_level=settings.log_level.value.lower(),
+            factory=True,
+        )
 
 
 if __name__ == "__main__":
--- atlas_backend/atlas_backend/core/router.py
+++ atlas_backend/atlas_backend/core/router.py
@@ -3,4 +3,4 @@ from atlas_backend.api.health.controller import health_router
 
 core_router = APIRouter()
 
-core_router.include_router(health_router,prefix="/health")
+core_router.include_router(health_router, prefix="/health")
